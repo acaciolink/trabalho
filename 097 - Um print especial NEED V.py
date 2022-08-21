15bac724929fe9593c5036dee2a8cb81037b8cb1
acaciolink <105466407+acaciolink@users.noreply.github.com>
2022-08-21 20:13:49 -0300

    formatando nomes

---
+++ 097 - Um print especial NEED V.py
@@ -0,0 +1,10 @@
+def escreva(msg):
+    t = len(msg) + 5
+    print('-' * t)
+    print(f'  {msg}')
+    print('-' * t)
+
+
+escreva('GUSTAVO GUANABARA')
+escreva('CURSO DE PYTHON NO YOUTUBE')
+escreva('CURSO EM VÍDEO')
