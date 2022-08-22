b4024dca2ff8d7d08185e33920e21b345a3ad4f1
acaciolink <105466407+acaciolink@users.noreply.github.com>
2022-08-21 21:52:55 -0300

    Add files via upload

---
+++ 002 - sintaxe.py
@@ -0,0 +1,12 @@
+one=int(input(' digite primeiro numero  '))
+two=int(input('digite segundo numero  '))
+print( 'primeiro numero ', one , 'difite segundo numero' , two , ' e veja as variações'  )
+
+print ('soma = ', one + two)
+print ('subtração = ' , one - two)
+print ('multiplicação = ' , one * two)
+print ( 'divisão inteira= ' ,  (one // two) )
+print ( 'divisão  = ', float (one / two))
+print ( 'poncia = ', float (one**two))
+print ( 'resto da divisão = ', float (one%two))
+
---
+++ 003 mp3 com playsound.py
@@ -0,0 +1,2 @@
+from playsound import playsound
+playsound("amor.mp3")
