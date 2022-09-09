091c43babb815283b6b4b970b84147c643601cd2
acaciolink <105466407+acaciolink@users.noreply.github.com>
2022-09-08 22:12:03 -0300

    Add files via upload

---
+++ 054 - Grupo da Maioridade.py
@@ -0,0 +1,17 @@
+while(True):
+    from datetime import date
+    atual = date.today().year
+    totmaior = 0
+    totmenor = 0
+    for pess in range(1, 8):
+        nasc = int(input('Digite o ano que {}° pessoa nasceu? '.format(pess)))
+        idade =atual - nasc
+        #print(' Essa pessoa tem {} anos'.format(idade))
+        if idade >= 21:
+            totmaior += 1
+        else:
+            totmenor += 1
+    print ('Ao todo tivemos {} pessoas maiores de idade'.format(totmaior))
+    print('E tambem tivemos  {} pessoas menores de idade'.format(totmenor))
+    
+    print('')
