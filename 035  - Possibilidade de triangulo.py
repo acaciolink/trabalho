d8ce13d1eb579e3e0e26f7ec8926cfc5eae0bb1d
acaciolink <105466407+acaciolink@users.noreply.github.com>
2022-09-26 00:28:43 -0300

    Add files via upload

---
+++ 035  - Possibilidade de triangulo.py
@@ -0,0 +1,11 @@
+print('-='*20)
+print(' Analizador de Triangulos')
+print('=-'*20)
+r1 = float(input(' Primeiro segmento: '))
+r2 = float(input(' Segundo segmento: '))
+r3 = float(input(' Terceiro segmento: '))
+if r1 < r2 +r3 and r2 < r1 +r3 and r3 < r1 +r2:
+    print(' Os segmentos FORMAM um triangulo!')
+else:
+    print(' Os segmentos NÃO PODEM formar um triangulo!')
+input()
