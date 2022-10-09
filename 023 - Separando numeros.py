2490d4704faf37dcd676a6d5ca7b440ecdcff840
acaciolink <105466407+acaciolink@users.noreply.github.com>
2022-10-09 02:31:01 -0300

    Add files via upload

---
+++ 023 - Separando numeros.py
@@ -0,0 +1,11 @@
+num = int(input('Informe um numero: '))
+n = str(num)
+print(' Analizando o número {} '.format(num))
+print(' Unidade: {} '.format(n[3]))
+print(' Dezena: {} '.format(n[2]))
+print(' Centena: {} '.format(n[1]))
+print(' Milhar: {} '.format(n[0]))
+#problema resolvido na 2v
+
+
+
