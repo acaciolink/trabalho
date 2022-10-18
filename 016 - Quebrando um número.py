bf4598817b46200898fd8102fd10cb51dd202376
acaciolink <105466407+acaciolink@users.noreply.github.com>
2022-10-18 07:48:47 -0300

    Add files via upload

---
+++ 016 - Quebrando um número.py
@@ -0,0 +1,5 @@
+'''from math import trunc
+n= float(input('Digite um numero: '))
+print ('O valor digitado foi {} e sua inteira é {} ' .format(n, trunc(n)))'''
+n= float(input('Digite um numero: '))
+print ('O valor digitado foi {} e sua inteira é {} ' .format(n, int(n)))
