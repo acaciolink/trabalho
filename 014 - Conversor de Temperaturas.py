e9f3ce21ddb79a3037ada34f5c4ca3748f200b79
acaciolink <105466407+acaciolink@users.noreply.github.com>
2022-10-20 16:03:55 -0300

    Add files via upload

---
+++ 014 - Conversor de Temperaturas.py
@@ -0,0 +1,3 @@
+c= float(input(' Informe a temperatura em °C: '))# SIMBOLO DE GRAU - ALT GR + ?
+f= 9 * c / 5 + 32        # nao usei parenteses porque a ordem ja estava correta
+print('A temperatura de {}°c correspondente a {}°F ' .format(c, f))
