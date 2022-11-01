d8c5ee464e87c8e91067e164a490d56bacb42542
acaciolink <105466407+acaciolink@users.noreply.github.com>
2022-11-01 00:05:01 -0300

    Add files via upload

---
+++ 006 - Dobro, Triplo, Raiz Quadrada.py
@@ -0,0 +1,9 @@
+n = int(input(' Digite um número : '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print(' o doblo de {} vale {},' .format(n , d))
+print (' o triplo de {}  vale {}. \nA raiz quadrada de {} vale {:.2f}. ' .format(n,t,n,r))
+#print (' o triplo de {}  vale {}. \nA raiz quadrada de {:.2f\\ mostra 2 casas decimais} vale {}. ' .format(n,t,n,r))
+#print (' o triplo de {}  vale {}. \nA raiz quadrada de {:.2f} vale {}. ' .format(n,(n*3),n,(n**(1/2)))) CALCULO SEM VARIAVEL
+#print (' o triplo de {}  vale {}. \nA raiz quadrada de {:.2f\\ mostra 2 casas decimais} vale {}. ' .format(n,t,n, pow(n, (1/2))))
